2b30cea2be786188fef9becde85665743165f2ce
AlessioDPT <alessio.recos@gmail.com>
2021-05-31 19:33:53 +0200

    ADD- cognomi

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ console.log ("email inserita: ", emailUser);
 // controlla che sia nella lista di chi può accedere:
 var emailList = ["pinko.pallino@email.it", "panko.pallino@email.it", "pippo.pallino@email.it", "pluto.pallino@email.it"];
 
-
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
 for (i = 0; i < emailList.length; i++) {
 
     if (emailUser != emailList[i]) {
@@ -26,3 +27,25 @@ for (i = 0; i < emailList.length; i++) {
         break;
     }
 }
+
+
+
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+
+// chiedi all’utente il cognome
+var userSurname = prompt("inserisci qui il TUO COGNOME");
+console.log ("Cognome inserito: ", userSurname);
+
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var surnameList = ["bianchi", "neri", "rossi", "verdi","gialli"];
+surnameList.push(userSurname);
+
+// stampa la lista ordinata alfabeticamente
+console.log(surnameList.sort()) ;
+document.getElementById("user_surname_list").innerHTML = "--x " + surnameList.sort() + " x--";
+
