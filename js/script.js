a8b3371aec9b288d1acd1c5a040489719d955488
AlessioDPT <alessio.recos@gmail.com>
2021-06-01 09:57:26 +0200

    COR.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
 // Chiedi all’utente la sua email:
 var emailUser = prompt("inserisci qui la TUA EMAIL");
 console.log ("email inserita: ", emailUser);
+console.log(emailUser, " : Mi Dispiace, qualcosa è andato storto");
 
 // controlla che sia nella lista di chi può accedere:
 var emailList = ["pinko.pallino@email.it", "panko.pallino@email.it", "pippo.pallino@email.it", "pluto.pallino@email.it"];
@@ -17,16 +18,15 @@ var emailList = ["pinko.pallino@email.it", "panko.pallino@email.it", "pippo.pall
 for (i = 0; i < emailList.length; i++) {
 
     if (emailUser != emailList[i]) {
-        console.log(emailUser, " : Mi Dispiace, qualcosa è andato storto");
         document.getElementById("esito_login").innerHTML = "\"" + emailUser + "\"" + " Mi Dispiace, qualcosa è andato storto :("
         
     }
     else {
-        console.log("BENTORNATO!");
         document.getElementById("esito_login").innerHTML = "CIAO!  \"" + emailUser + "\"  BENTORNATO!"
         break;
     }
 }
+console.log("BENTORNATO!");
 
 
 
